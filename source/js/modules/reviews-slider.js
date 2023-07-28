1292fc559630b439c829ad91c0d6ca538e5d4631
Igor <trololol-lolo@mail.ru>
2023-07-29 02:03:23 +1000

    Правки по Отзывам

--- source/js/modules/reviews-slider.js
+++ source/js/modules/reviews-slider.js
@@ -3,12 +3,11 @@ const reviewsSlider = document.querySelector('.reviews__slider');
 
 export const reviewSlider = () => {
   reviewsSlider &&
-  new Swiper('.swiper', {
+  new Swiper('.swiper2', {
     navigation: {
       nextEl: '.reviews__control--next',
       prevEl: '.reviews__control--prev',
     },
     slidesPerView: 1,
-    loop: true,
   })
 }
